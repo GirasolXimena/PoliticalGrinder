44ad01716743261e6071c1ec6bd59040823a5e15
Danny Yang <dannyyaaj@gmail.com>
2018-09-23 11:59:05 -0500

    test legiscan API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.legiscan.com/?key=476905b116d0b72a1baefebdbb1e5792&op=getMasterList&state=MN`)
+      .then(response => console.log(response.data.masterlist))
+      .catch((error) => console.log(error));
+  }
+
   render() {
+    console.log()
     return (
       <div className="App">
         <header className="App-header">
